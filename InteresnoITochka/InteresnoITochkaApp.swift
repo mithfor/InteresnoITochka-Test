0e8196672b9c072b7174fef78fc41bd5f515e2ea
Dmitriy Voronin <devoronin@gmail.com>
2025-08-27 09:04:31 +0300

    Initial Commit

---
+++ InteresnoITochka/InteresnoITochkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteresnoITochkaApp.swift
+//  InteresnoITochka
+//
+//  Created by Dmitrii Voronin on 27.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct InteresnoITochkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
